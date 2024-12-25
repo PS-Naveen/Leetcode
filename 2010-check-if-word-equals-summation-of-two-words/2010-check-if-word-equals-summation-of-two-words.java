20e0ab075bfc6b8f1d417b7c815eb43584c237b0
Naveen Kumar PS <156183344+PS-Naveen@users.noreply.github.com>
2024-12-25 10:33:47 +0530

    Time: 6 ms (8.68%), Space: 42 MB (6.95%) - LeetHub

--- 2010-check-if-word-equals-summation-of-two-words/2010-check-if-word-equals-summation-of-two-words.java
+++ 2010-check-if-word-equals-summation-of-two-words/2010-check-if-word-equals-summation-of-two-words.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        
+        String fW = "";
+		String sW = "";
+		String tW = "";
+		for (int i = 0 ; i < firstWord.length() ; i++) {
+		    fW = fW + ((int) firstWord.charAt(i) - 97);
+		}
+		for (int i = 0 ; i < secondWord.length() ; i++) {
+		    sW = sW + ((int) secondWord.charAt(i) - 97);
+		}
+		for (int i = 0 ; i < targetWord.length() ; i++) {
+		    tW = tW + ((int) targetWord.charAt(i) - 97);
+		}
+		int first = Integer.parseInt(fW);
+		int second = Integer.parseInt(sW);
+		int target = Integer.parseInt(tW);
+		if(first + second == target)   return true;
+		return false;
     }
 }
