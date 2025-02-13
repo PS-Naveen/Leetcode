a49b49df1b44793b84673c03688b61068dc82b4a
Naveen Kumar PS <156183344+PS-Naveen@users.noreply.github.com>
2025-02-13 08:08:21 +0530

    Time: 9 ms (14.39%), Space: 45.7 MB (7.59%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.java
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -1,21 +1,14 @@
 class Solution {
     public String finalString(String s) {
-        String out = "";
-        for(int i = 0 ; i < s.length() ; i++) {
-            if(s.charAt(i) == 'i'){
-                out = reverse(out);
-            } else {
-                out += s.charAt(i);
+        String ans = "";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='i'){
+                ans = new StringBuilder(ans).reverse().toString();
+            }
+            else{
+                ans = ans+s.charAt(i);
             }
         }
-        return out;
-    }
-
-    public String reverse (String rev) {
-        String out = "";
-        for(int i = rev.length()-1 ; i >= 0; i--) {
-            out += rev.charAt(i);
-        }
-        return out;
+        return ans;
     }
 }
