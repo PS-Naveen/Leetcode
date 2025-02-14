f9f14bde873f0e39ee984e2a89c33efaf3927a45
Naveen Kumar PS <156183344+PS-Naveen@users.noreply.github.com>
2025-02-14 08:37:00 +0530

    Time: 5 ms (53.93%), Space: 43 MB (89.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,15 @@
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())   return false; 
-        int[] arr = new int[26];
+        if(s.length() != t.length())    return false;
+        int arr[] = new int[26];
         for(int i = 0 ; i < s.length() ; i++){
             arr[s.charAt(i) - 'a']++;
             arr[t.charAt(i) - 'a']--;
         }
-        for(int count : arr){
-            if(count != 0)  return false;
+        for(int ar : arr){
+            if(ar != 0)    return false;
         }
         return true;
     }
 }
-/*time complexity : O(n)
-space complexity : O(1))*/
