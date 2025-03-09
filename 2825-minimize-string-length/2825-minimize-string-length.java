63a7954a683d48c10470ce37a2fb660b5fdd7b10
Naveen Kumar PS <156183344+PS-Naveen@users.noreply.github.com>
2025-03-10 00:01:48 +0530

    Time: 7 ms (80.65%), Space: 45.7 MB (13.82%) - LeetHub

--- 2825-minimize-string-length/2825-minimize-string-length.java
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -1,13 +1,9 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        HashSet<Character> ch = new HashSet<>();
-        int count = 0;
-        for(int i = 0 ; i < s.length() ; i++) {
-            if(!ch.contains(s.charAt(i))){
-                ch.add(s.charAt(i));
-                count++;
-            }
+        HashSet<Character> hs = new HashSet<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            hs.add(s.charAt(i));
         }
-        return count;
+        return hs.size();
     }
 }
