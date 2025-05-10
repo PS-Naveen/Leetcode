c314a02031b747f41d7657dbe9124b05632a31cc
Naveen Kumar PS <156183344+PS-Naveen@users.noreply.github.com>
2025-05-10 19:41:59 +0530

    Time: 46 ms (5.49%), Space: 45.8 MB (7.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,13 +2,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())    return false;
-        int arr[] = new int[26];
+        HashMap<String , Integer> hashMap1 = new HashMap<>();
+        HashMap<String , Integer> hashMap2 = new HashMap<>();
         for(int i = 0 ; i < s.length() ; i++){
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+            if(!hashMap1.containsKey(s.charAt(i) + "")){
+                hashMap1.put(s.charAt(i) + "", 1);
+            } else {
+                int val = hashMap1.get(s.charAt(i) + "");
+                hashMap1.put(s.charAt(i) + "", val+1);
+            }
+            if(!hashMap2.containsKey(t.charAt(i) + "")){
+                hashMap2.put(t.charAt(i) + "", 1);
+            } else {
+                int val = hashMap2.get(t.charAt(i) + "");
+                hashMap2.put(t.charAt(i) + "", val+1);
+            }
         }
-        for(int ar : arr){
-            if(ar != 0)    return false;
+        for(int i = 0 ; i < s.length() ; i++){
+            String key = s.charAt(i) + "";
+            if(!(hashMap1.containsKey(key) && hashMap2.containsKey(key)))   return false;
+            int hm1 = hashMap1.get(key);
+            int hm2 = hashMap2.get(key);
+            if(hm1 != hm2){
+                return false;
+            }
         }
         return true;
     }
